5d708e40c23716d2aa9d243322d171d9f75cd380
David Anderson <david.n.anderson@gsa.gov>
2024-10-16 14:37:55 -0400

    adds job for checking deduplication is turned on; cleans up on_success triggers

---
+++ ci/set-deduplication.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Send GET request for user profile
+
+curl -X GET ${DEFECTDOJO_SYSTEM_SETTINGS_URL} \
+  -H "accept: application/json" \
+  -H "Authorization: Token $DEFECTDOJO_AUTH_TOKEN" > response.json
+
+deduplication_status=$(cat response.json | jq '.results[].enable_deduplication')
+
+if [[ $deduplication_status != "true" ]]; then
+  echo "deduplication is off"
+  echo "turning deduplication on"
+  curl -X PATCH ${DEFECTDOJO_SYSTEM_SETTINGS_URL}1/ \
+  -H "accept: application/json" \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Token $DEFECTDOJO_AUTH_TOKEN" \
+  -d '{"enable_deduplication": true}' > result.json
+
+  deduplication_status=$(cat result.json | jq '.results[].enable_deduplication')
+  if [[ $deduplication_status != "true" ]]; then
+    echo "error turning deduplication on"
+    exit 1;
+  fi
+fi
+
+echo "deduplication is on"
